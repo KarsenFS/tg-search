dadd49e404c59648e371712787bab1cb044a8ec1
Руслан <ruslanasadov32@gmail.com>
2020-03-07 15:19:53 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div style="text-align:center">
+      TgSearch
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
